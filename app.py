d6bc93d1980b82bf68fa45eff58c66af70ecea85
Kali Junior Brown <browndesmond30@yahoo.com>
2022-02-18 10:27:09 +0000

    Added code changes to run tests for different projects by just using config scripts and sending emails afterwards

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
+import subprocess
+from subprocess import PIPE
 import os
 from pathlib import Path
-
-import git as gitMod
-from git import Repo
+from datetime import datetime
+import base64
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import (
+    Mail,
+    Attachment,
+    FileContent,
+    FileName,
+    FileType,
+    Disposition,
+)
 
 from flask import Flask, request
 
@@ -11,7 +21,7 @@ app = Flask(__name__)
 
 PORT = "8000"
 
-DEBUG = True
+DEBUG = False  # Change to True during dev
 
 BASE_WORK_DIR = (
     str(Path.home()) if DEBUG else "/var/www/sites/"
@@ -27,38 +37,70 @@ def index():
     return "Heya! It's Gradia's CI Implementation on Pythonanywhere 😃"
 
 
-"""
-List of things to do upon push notification:
-    - pull updated stream
-    - activate virtualenv
-    - run test
-    - send email to contributors if there are any failures (i.e for V1. After V1 prob send test summary whether pass or fail)
-"""
+CONTRIBUTORS_EMAIL = (
+    "conradalwinho@gmail.com",
+    "browndesmond30@yahoo.com",
+    "desmond@gradia.net",
+    "moseszeggey@yahoo.com",
+    "davidyevu18@gmail.com",
+    "jkwokchunkan@gmail.com",
+)
 
 
-# TODO: Fill out this function. (Does nothing now.)
-def send_emails(contributor_emails=[]):
+def send_email_notifications(output_file_path):
     """
     Sends emails to contributors
-    :param contributor_emails:
+    :param output_file_path:
     :returns:
     """
-    pass
+    message = Mail(
+        from_email="Gradia CI Platform <info@gradia.net>",
+        to_emails=CONTRIBUTORS_EMAIL,
+        subject="Test running report",
+        html_content="<p>This email contains an attachment of the file containing the test run logs</p>",
+    )
+
+    # Technically could has read as not byte that way no need for decoding but this to allow for attachment other file types like pds, etc
+    with open(output_file_path, "rb") as file:
+        data = file.read()
+
+    encoded_file_content = base64.b64encode(data).decode()
+
+    file_name = os.path.basename(output_file_path)
+
+    attachment = Attachment(
+        FileContent(encoded_file_content),
+        FileName(file_name),
+        FileType("application/txt"),
+        Disposition("attachment"),
+    )
+
+    message.attachment = attachment
+
+    sender_instance = SendGridAPIClient(
+        "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"
+    )  # os.environ.get("SENDGRID_API_KEY"))
+    try:
+        return sender_instance.send(message)
+    except Exception as e:
+        print(e)  # .message)  # In V2, prob log the error
 
 
-# TODO: Fill out this function. (Dummy now)
-def run_tests():
+def log_output(output):
     """
-    Runs tests and pipe the output into a file
+    Saves output to a file and returns the file path
+    :param output:
     :returns:
     """
-    # Run some tests
-    # Inspect the test and provide summary on:
-    #  - number of test cases
-    #  - number passes
-    #  - number failures
-    # returns this information
-    pass
+    save_directory_path = os.path.join(BASE_WORK_DIR, ".configs")
+    if not os.path.exists(save_directory_path):
+        os.mkdir(save_directory_path)
+
+    file_name = f"test-output-{datetime.now().strftime('%d-%m-%y-%HH:%MM:%SS')}.txt"
+    with open(file_name, "w") as file:
+        file.write(output)
+
+    return file_name
 
 
 @app.route("/payload/", methods=["POST"])
@@ -67,16 +109,18 @@ def handle_webhooks():
     This is our webhooks handler. Current Implementation only supports push events
     :returns:
     """
-    clone_url = request.json["repository"]["clone_url"]
+    # clone_url = request.json["repository"]["clone_url"]
     repo_name = request.json["repository"]["name"]
 
-    project_directory = os.path.join(BASE_WORK_DIR, repo_name)
+    config_script = os.path.join(os.getcwd(), ".configs", f"{repo_name}-config.sh")
+    args = ["/bin/sh", config_script]
 
-    if not os.path.exists(project_directory):
-        Repo.clone_from(clone_url, project_directory)
+    p = subprocess.Popen(args, stdin=PIPE, stdout=PIPE)
+    string_output = p.stdout.read().decode()
 
-    git = gitMod.cmd.Git(project_directory)
-    git.pull()
+    output_path = log_output(string_output)
+
+    send_email_notifications(output_path)
 
     return {"status": "ok"}
 
