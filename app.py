a490b068095569e4a4e2a389d7db3e316b7e7937
Kali Junior Brown <browndesmond30@yahoo.com>
2022-03-16 14:35:24 +0000

    Optimized response time by implementing threaded way of executing test scripts

--- app.py
+++ app.py
@@ -2,17 +2,8 @@ import subprocess
 from subprocess import PIPE
 import os
 from pathlib import Path
-from datetime import datetime
-import base64
-from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import (
-    Mail,
-    Attachment,
-    FileContent,
-    FileName,
-    FileType,
-    Disposition,
-)
+
+from .script import ScriptExecutionThread
 
 from flask import Flask, request
 
@@ -37,88 +28,16 @@ def index():
     return "Heya! It's Gradia's CI Implementation on Pythonanywhere 😃"
 
 
-CONTRIBUTORS_EMAIL = (
-    "conradalwinho@gmail.com",
-    "browndesmond30@yahoo.com",
-    "desmond@gradia.net",
-    "moseszeggey@yahoo.com",
-    "davidyevu18@gmail.com",
-    "jkwokchunkan@gmail.com",
-)
-
-
-def send_email_notifications(output_file_path):
-    """
-    Sends emails to contributors
-    :param output_file_path:
-    :returns:
-    """
-    message = Mail(
-        from_email="Gradia CI Platform <info@gradia.net>",
-        to_emails=CONTRIBUTORS_EMAIL,
-        subject="Test running report",
-        html_content="<p>This email contains an attachment of the file containing the test run logs</p>",
-    )
-
-    # Technically could has read as not byte that way no need for decoding but this to allow for attachment other file types like pds, etc
-    with open(output_file_path, "rb") as file:
-        data = file.read()
-
-    encoded_file_content = base64.b64encode(data).decode()
-
-    file_name = os.path.basename(output_file_path)
-
-    attachment = Attachment(
-        FileContent(encoded_file_content),
-        FileName(file_name),
-        FileType("application/txt"),
-        Disposition("attachment"),
-    )
-
-    message.attachment = attachment
-
-    sender_instance = SendGridAPIClient(os.environ.get("SENDGRID_API_KEY"))
-    try:
-        return sender_instance.send(message)
-    except Exception as e:
-        print(e)  # .message)  # In V2, prob log the error
-
-
-def log_output(output):
-    """
-    Saves output to a file and returns the file path
-    :param output:
-    :returns:
-    """
-    save_directory_path = os.path.join(BASE_WORK_DIR, ".configs")
-    if not os.path.exists(save_directory_path):
-        os.mkdir(save_directory_path)
-
-    file_name = f"test-output-{datetime.now().strftime('%d-%m-%y-%HH:%MM:%SS')}.txt"
-    with open(file_name, "w") as file:
-        file.write(output)
-
-    return file_name
-
-
 @app.route("/payload/", methods=["POST"])
 def handle_webhooks():
     """
     This is our webhooks handler. Current Implementation only supports push events
     :returns:
     """
-    # clone_url = request.json["repository"]["clone_url"]
     repo_name = request.json["repository"]["name"]
 
-    config_script = os.path.join(BASE_WORK_DIR, ".configs", f"{repo_name}-config.sh")
-    args = ["/bin/sh", config_script]
-
-    p = subprocess.Popen(args, stdin=PIPE, stdout=PIPE)
-    string_output = p.stdout.read().decode()
-
-    output_path = log_output(string_output)
-
-    send_email_notifications(output_path)
+    execution_thread = ScriptExecutionThread(work_path=BASE_WORK_DIR, repo_name=repo_name)
+    execution_thread.start()
 
     return {"status": "ok"}
 
---
+++ script.py
@@ -0,0 +1,101 @@
+import subprocess
+from subprocess import PIPE
+from threading import Thread
+import base64
+import os
+from datetime import datetime
+
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import (
+    Mail,
+    Attachment,
+    FileContent,
+    FileName,
+    FileType,
+    Disposition,
+)
+
+
+CONTRIBUTORS_EMAIL = (
+    "conradalwinho@gmail.com",
+    "browndesmond30@yahoo.com",
+    "desmond@gradia.net",
+    "moseszeggey@yahoo.com",
+    "davidyevu18@gmail.com",
+    "jkwokchunkan@gmail.com",
+)
+
+
+def send_email_notifications(output_file_path: str) -> None:
+    """
+    Sends emails to contributors
+    :param output_file_path:
+    :returns:
+    """
+    message = Mail(
+        from_email="Gradia CI Platform <info@gradia.net>",
+        to_emails=CONTRIBUTORS_EMAIL,
+        subject="Test running report",
+        html_content="<p>This email contains an attachment of the file containing the test run logs</p>",
+    )
+
+    # Technically could has read as not byte that way no need for decoding but this to allow for attachment other file types like pds, etc
+    with open(output_file_path, "rb") as file:
+        data = file.read()
+
+    encoded_file_content = base64.b64encode(data).decode()
+
+    file_name = os.path.basename(output_file_path)
+
+    attachment = Attachment(
+        FileContent(encoded_file_content),
+        FileName(file_name),
+        FileType("application/txt"),
+        Disposition("attachment"),
+    )
+
+    message.attachment = attachment
+
+    sender_instance = SendGridAPIClient(os.environ.get("SENDGRID_API_KEY"))
+    try:
+        return sender_instance.send(message)
+    except Exception as e:
+        print(e)  # .message)  # In V2, prob log the error
+
+
+def log_output(output: str, base_path: str) -> str:
+    """
+    Saves output to a file and returns the file path
+    :param output:
+    :returns:
+    """
+    save_directory_path = os.path.join(base_path, ".configs")
+    if not os.path.exists(save_directory_path):
+        os.mkdir(save_directory_path)
+
+    file_name = f"test-output-{datetime.now().strftime('%d-%m-%y-%HH:%MM:%SS')}.txt"
+    with open(file_name, "w") as file:
+        file.write(output)
+
+    return file_name
+
+
+class ScriptExecutionThread(Thread):
+    def __init__(self, work_path: str, repo_name: str) -> None:
+        self.work_path = work_path
+        self.repo_name = repo_name
+
+    def run(self) -> None:
+        """
+        Runs the config script, logs the output and send email notifications to contributors
+        :returns:
+        """
+        shell_script_path = os.path.join(self.work_path, ".configs", f"{self.repo_name}-config.sh")
+        args = ["/bin/sh", shell_script_path]
+
+        p = subprocess.Popen(args, stdin=PIPE, stdout=PIPE)
+        string_output = p.stdout.read().decode()
+
+        output_path = log_output(string_output, self.work_path)
+
+        send_email_notifications(output_path)
