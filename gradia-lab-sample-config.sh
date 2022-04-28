52e80c81a268f1e8cdc8d1a21498613a295ffdf7
Desmond Brown <browndesmond30@yahoo.com>
2022-04-28 10:56:28 +0000

    Added sample of shell script to run the tests

---
+++ gradia-lab-sample-config.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+# To provide a little context on the nature of these shell configuration files:
+# This configuration file is specific to GRADIA specifications. i.e python version (3.9), 
+#  name requirements file requirements39.txt, etc
+
+
+# variables
+branch_name=$1
+project_base_directory_path=$2
+project_directory_path="${project_base_directory_path}gradia-lab-sample"
+underline="==================================================================="
+
+echo "branch name: $branch_name"
+echo "project base path: $project_base_directory_path"
+echo "project directory path: $project_directory_path"
+echo $underline 
+
+# create a virtualenv if it doesn't exist
+if [ ! -d "~/.environments/gradia-lab-sample/" ]; then 
+    echo "Creating python virtual environment..."
+    echo $underline
+    mkdir -p ~/.environments
+    python3 -m venv ~/.environments/gradia-lab-sample
+    echo $underline
+fi
+
+# active virtual env 
+echo "Activating python virtual environment.."
+echo $underline
+source ~/.environments/gradia-lab-sample/bin/activate
+echo $underline 
+
+if [! -d $project_directory_path ]; then 
+    # Do cloning here
+    echo "Project does not exit"
+    echo "Cloning Project..."
+    pushd "$project_base_directory_path"
+    git clone git@github.com:kali-physi-hacker/GRADIA_lab.git
+fi
+echo "changing directory to: $project_directory_path"
+pushd "$project_directory_path"
+git remote -v
+
+echo "Checkout to branch: $branch_name..."
+echo $underline
+git checkout -f $branch_name
+echo $underline 
+
+# Pulling recent changes
+echo "Pulling changes..."
+echo $underline 
+git pull upstream $branch_name  # on pythonanywhere this will be `git pull origin frontend-looser-ratelimit`
+echo $underline 
+
+# install requirements for both fresh and updated dependency instances 
+echo "Installing python dependencies..."
+echo $underline
+pip install -r requirements39.txt
+echo $underline 
+
+# run frontend dependency install 
+echo "Installing frontend (react) dependencies..."
+echo $underline
+cd frontend && npm install
+echo $underline
+
+# running frontend build 
+echo "Building frontend (react) files..."
+echo $underline
+npm run build 
+echo $underline
+
+
+# Testing comes here
+
+# linting frontend
+echo "Checking linting..."
+echo $underline
+npm run lint
+echo $underline 
+
+# linting backend 
+echo "Checking black formatting"
+echo $underline 
+cd .. && black --line-length 117 --exclude '^.*\b(migrations)\b.*$' --check django_backend selenium_tests
+echo $underline 
+
+# unittests
+echo "Running django unittests"
+echo $underline 
+cd django_backend && pytest 
+echo $underline 
+
+# # selenium test
+# cd ../selenium_tests && pytest 
+
