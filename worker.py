eee3d4e9dc94e0815b1448c0a83ec25ac41645a7
Desmond Brown <browndesmond30@yahoo.com>
2022-05-06 20:46:32 +0000

    Changed redis to use redislab in cloud

--- worker.py
+++ worker.py
@@ -1,4 +1,4 @@
-import os 
+from urllib.parse import quote
 
 import redis 
 from rq import Worker, Queue, Connection 
@@ -6,7 +6,8 @@ from rq import Worker, Queue, Connection
 
 listen = [ "default"]
 
-redis_url = os.getenv("REDISTOGO_URL", "redis://localhost:6379")
+encoded_password = quote("MXQenWA5B4htsQ@", safe="")
+redis_url = f"redis://desmondbrown:{encoded_password}@redis-14955.c257.us-east-1-3.ec2.cloud.redislabs.com:14955"  # os.getenv("REDISTOGO_URL", "redis://localhost:6379")
 
 
 conn = redis.from_url(redis_url)
