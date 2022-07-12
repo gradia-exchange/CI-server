b49fd7fda624bfea091db0268bb25908f8950191
Kali Junior Brown <browndesmond30@yahoo.com>
2022-07-12 21:03:46 +0000

    Implemented worker to work in dev (debug) mode to use local redis server

--- worker.py
+++ worker.py
@@ -1,3 +1,6 @@
+import argparse 
+
+from distutils.sysconfig import get_config_h_filename
 from urllib.parse import quote
 
 import redis
@@ -6,14 +9,30 @@ from rq import Worker, Queue, Connection
 
 listen = ["default"]
 
+PRODUCTION = "production"
+DEVELOPMENT = "development"
+
 encoded_password = quote("MXQenWA5B4htsQ@", safe="")
 redis_url = f"redis://desmondbrown:{encoded_password}@redis-14955.c257.us-east-1-3.ec2.cloud.redislabs.com:14955"  # os.getenv("REDISTOGO_URL", "redis://localhost:6379")
 
 
-conn = redis.from_url(redis_url)
+def get_connection(debug=False):
+    if debug:
+        return redis.from_url("redis://localhost:6379")
+    return redis.from_url(redis_url)
 
 
+conn = get_connection(debug=True)
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--mode", default=PRODUCTION, required=False, help="production vs development mode")
+    args = parser.parse_args()
+
+    debug = False 
+    if args.mode == DEVELOPMENT:
+        debug = True 
+
+    conn = get_connection(debug=debug)
     with Connection(conn):
         worker = Worker(list(map(Queue, listen)))
         worker.work()
