9adaefa3c438d88b4b0a93663f2ed757bc2cd244
Kali Junior Brown <browndesmond30@yahoo.com>
2022-07-12 20:53:58 +0000

    webhooks handler to handle pull requests

--- app.py
+++ app.py
@@ -30,10 +30,17 @@ def handle_webhooks():
     :returns:
     """
     # get repo name
-    repo_name = request.json["repository"]["name"]
-    branch_name = request.json["ref"].split("/")[-1]
-    owner = request.json["pusher"]["name"]
-    commit_hash = request.json["head_commit"]["id"]
+    event = request.headers.get("X-GITHUB-EVENT")
+    if event == "pull_request":
+        repo_name = request.json["pull_request"]["head"]["repo"]["name"]
+        branch_name = request.json["pull_request"]["head"]["ref"]
+        owner = request.json["pull_request"]["head"]["user"]["login"]
+        commit_hash = request.json["pull_request"]["head"]["sha"]
+    else:  # There prob may be other events that sends diff request data?
+        repo_name = request.json["repository"]["name"]
+        branch_name = request.json["ref"].split("/")[-1]
+        owner = request.json["pusher"]["name"]
+        commit_hash = request.json["head_commit"]["id"]
 
     execution_thread = TestRunner(
         owner=owner,
