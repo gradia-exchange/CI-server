5d94866dd937fb3815af8a453dfc490fde776021
Kali Junior Brown <browndesmond30@yahoo.com>
2022-07-15 09:16:42 +0000

    Fixed forgotten debug param for worker redis connection

--- worker.py
+++ worker.py
@@ -22,7 +22,7 @@ def get_connection(debug=False):
     return redis.from_url(redis_url)
 
 
-conn = get_connection(debug=True)
+conn = get_connection()
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-m", "--mode", default=PRODUCTION, required=False, help="production vs development mode")
