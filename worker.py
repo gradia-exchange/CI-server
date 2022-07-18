9baf156cca334dc27c6daef59aa50a3340c40568
Desmond Brown <browndesmond30@yahoo.com>
2022-07-18 09:03:11 -0700

    Removed redundant code and modified it to use production redis-url

--- worker.py
+++ worker.py
@@ -9,30 +9,23 @@ from rq import Worker, Queue, Connection
 
 listen = ["default"]
 
-PRODUCTION = "production"
-DEVELOPMENT = "development"
+# PRODUCTION = "production"
+# DEVELOPMENT = "development"
 
 encoded_password = quote("MXQenWA5B4htsQ@", safe="")
 redis_url = f"redis://desmondbrown:{encoded_password}@redis-14955.c257.us-east-1-3.ec2.cloud.redislabs.com:14955"  # os.getenv("REDISTOGO_URL", "redis://localhost:6379")
 
 
-def get_connection(debug=False):
-    if debug:
-        return redis.from_url("redis://localhost:6379")
-    return redis.from_url(redis_url)
-
-
-conn = get_connection()
+conn = redis.from_url(redis_url)
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-m", "--mode", default=PRODUCTION, required=False, help="production vs development mode")
-    args = parser.parse_args()
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("-m", "--mode", default=PRODUCTION, required=False, help="production vs development mode")
+    # args = parser.parse_args()
 
-    debug = False 
-    if args.mode == DEVELOPMENT:
-        debug = True 
+    # debug = False 
+    # if args.mode == DEVELOPMENT:
+    #     debug = True 
 
-    conn = get_connection(debug=debug)
     with Connection(conn):
         worker = Worker(list(map(Queue, listen)))
         worker.work()
