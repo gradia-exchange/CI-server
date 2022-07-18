4bcf7c42221084ffc8d9a75ff254312b9f964218
Desmond Brown <browndesmond30@yahoo.com>
2022-07-18 09:03:35 -0700

    Modified run_script to simulate github checks

--- run_script.py
+++ run_script.py
@@ -2,6 +2,7 @@ import subprocess
 import sys
 import os
 import re
+import glob
 
 from dotenv import load_dotenv
 
@@ -12,36 +13,82 @@ configs_path = os.environ.get("CONFIGS_PATH")
 project_base_directory = os.environ.get("PROJECT_PATH")
 author = sys.argv[1]
 project_name = sys.argv[2]
-shell_script = f"{os.path.join(configs_path, project_name)}-config.sh"
+
+
+def get_last_line(output):
+    lines = output.decode("utf-8").split("\n")
+    if lines[-1] == "":
+        return lines[-2]
+    
+    return lines[-1]
+
 
 def run_shell_script():
-    args = [
-        "/bin/bash",
-        shell_script,
-        author,
-        "master",
-        project_base_directory,
-    ]
 
-    process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    context = ""
-    while True:
-        output = process.stdout.readline().decode().strip()
-        if output == "" and process.poll() is not None:
-            break 
-        if output:
-            # check for the line and find the context
-            context_found = re.match(r"^context: ", output)
-            if context_found:
-                print("Found:", output.lstrip("context:"))
-            print(output.strip())
+    # Get all the scripts to run and use as test contexts
+    shell_scripts_path = os.path.join(configs_path, project_name)
+    scripts = sorted(glob.glob(f"{shell_scripts_path}/*.sh"))
+    contexts = [os.path.basename(script)[:-3].split("-")[-1] for script in scripts]
+
+    # simulating github checks
+    for context in contexts:
+        print(context, ": pending")
+
+    dependency_scripts = ["setup", "dependency_install"]
+    print("All contexts:", contexts)
+
+    for index, script in enumerate(scripts):
+        current_context = contexts[index]
+
+        args = [
+            "/bin/bash", 
+            script,
+            author,
+            "master",
+            project_base_directory
+        ]
+        process = subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        if process.returncode != 0:
+            # send a github error check
+            print("errored at:", current_context)
+            if current_context in dependency_scripts:
+                # send failed for all the other ones
+                for context in contexts[index+1:]:
+                    print("did not run:", context)
+                    # github_checks.create_status(status=github_checks.ERROR, context=context, description="Your tests failed on GradiaCI!")
+                break
+            continue 
+
+        # get the last line of the ouput and if it echo's success then send a github pass else send failed
+        status = get_last_line(output=process.stdout)
+        if status == "status: success":
+            print(f"{current_context}: success")
+            # github_checks.create_status(status=github_checks.SUCCESS, context=current_context, description="Your tests passed on GradiaCI!")
+        else:
+            print(f"{current_context}: failed")
+            # github_checks.create_status(status=github_checks.ERROR, context=current_context, description="Your tests failed on GradiaCI!")
+
+        with open("test-output.txt", "a") as output_file:
+            output_file.write(process.stdout.decode("utf-8"))
+
+
+        # while True:
+        #     output = process.stdout.readline().decode().strip()
+        #     if output == "" and process.poll() is not None:
+        #         break 
+        #     if output:
+        #         # check for the line and find the context
+        #         context_found = re.match(r"^context: ", output)
+        #         if context_found:
+        #             print("Found:", output.lstrip("context:"))
+        #         print(output.strip())
 
 
     
 
-    output_stream = subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    with open("test-output.txt", "w") as output_file:
-        output_file.write(output_stream.stdout.decode("utf-8"))
+    # output_stream = subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    # with open("test-output.txt", "w") as output_file:
+    #     output_file.write(output_stream.stdout.decode("utf-8"))
 
 
 output = run_shell_script()
